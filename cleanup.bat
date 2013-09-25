70a0768d84c4e2b6d6d3c5ae33dfbcdc9393e61a
Mark <xiaoxid@163.com>
2013-09-25 10:43:39 -0500

    init commit

---
+++ cleanup.bat
@@ -0,0 +1,19 @@
+del /s *.bak
+del /s *.dep
+del /s *.crf
+del /s *.o
+del /s *.d
+del /s *.lnp
+del /s *.plg
+del /s *.htm
+del /s *.plg
+del /s *.tra
+del /s *.lst
+del /s *.m51
+del /s *.obj
+del /s *.#1
+del F93x_PWM
+del tmp.out
+del cyglink.txt
+
+
